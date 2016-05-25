4949c69279385e703a273869833764fbef9026c6
Ricardo Lara <ricardo.lara06@hotmail.com>
2016-05-24 23:54:41 -0500

    se crea Ingreso funcional con base de datos

---
+++ ProyectoSiis2/LayerBusiness/LayerBusinessElementos.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace LayerBusiness
+{
+    public class LayerBusinessElementos
+    {
+        LayerData.LayerDataElementos ObjBS = new LayerData.LayerDataElementos();
+        public int InsertarElementos(Int64 IdElemento, Int64 NumeroPlaca, Int64 NumeroSerial, string Marca, string Modelo, string Descripcion)
+        {
+            try
+            {
+                return ObjBS.InsertarElementos(IdElemento, NumeroPlaca, NumeroSerial, Marca, Modelo, Descripcion);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                ObjBS = null;
+            }
+        }
+
+
+        public DataTable MostrarElementos()
+        {
+            try
+            {
+                return ObjBS.MostrarElementos();
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                ObjBS = null;
+            }
+
+        }
+        public int EditarElementos(Int64 IdElemento, Int64 NumeroPlaca, Int64 NumeroSerial, string Marca, string Modelo, string Descripcion)
+        {
+            try
+            {
+                return ObjBS.EditarElementos(IdElemento, NumeroPlaca, NumeroSerial, Marca, Modelo, Descripcion);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                ObjBS = null;
+            }
+        }
+        public int EliminarElemento(Int64 IdElemento)
+        {
+            try
+            {
+                return ObjBS.EliminarElemento(IdElemento);
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                ObjBS = null;
+            }
+
+        }
+
+
+    }
+}
---
+++ ProyectoSiis2/LayerData/LayerDataElementos.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LayerData
+{
+    public class LayerDataElementos
+    {
+        public string strconn = @"Data Source=ACER;Initial Catalog=Proyecto;Integrated Security=True";
+
+        public LayerDataElementos() { }
+
+        public int InsertarElementos(Int64 IdElemento, Int64 NumeroPlaca, Int64 NumeroSerial, string Marca, string Modelo, string Descripcion)
+        {
+            using (SqlConnection cnx = new SqlConnection(strconn))
+            {
+                cnx.Open();
+                SqlCommand OrdenSQL = new SqlCommand("SpInsertarElementos", cnx);
+                OrdenSQL.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    OrdenSQL.Parameters.AddWithValue("@IdElemento", IdElemento);
+                    OrdenSQL.Parameters.AddWithValue("@NumeroPlaca", NumeroPlaca);
+                    OrdenSQL.Parameters.AddWithValue("@NumeroSerial ", NumeroSerial);
+                    OrdenSQL.Parameters.AddWithValue("@Marca", Marca);
+                    OrdenSQL.Parameters.AddWithValue("@Modelo", Modelo);
+                    OrdenSQL.Parameters.AddWithValue("@Descripcion", Descripcion);
+                    return OrdenSQL.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    cnx.Close();
+                    cnx.Dispose();
+                    OrdenSQL.Dispose();
+                }
+            }
+        }
+        public DataTable MostrarElementos()
+        {
+            using (SqlConnection cnx = new SqlConnection(strconn))
+            {
+                cnx.Open();
+                SqlDataAdapter ObjMostrar = new SqlDataAdapter("SpMostrarElementos", cnx);
+                ObjMostrar.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataSet ds = new DataSet();
+                try
+                {
+                    ObjMostrar.Fill(ds, "Table");
+                    return ds.Tables["Table"];
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+
+
+                    cnx.Close();
+                    cnx.Dispose();
+                    ObjMostrar.Dispose();
+                    ds.Dispose();
+                }
+            }
+        }
+        public int EditarElementos(Int64 IdElemento, Int64 NumeroPlaca, Int64 NumeroSerial, string Marca, string Modelo, string Descripcion)
+        {
+            using (SqlConnection cnx = new SqlConnection(strconn))
+            {
+                cnx.Open();
+
+                SqlCommand OrdenSQL = new SqlCommand("SpEditarElementos", cnx);
+                OrdenSQL.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    OrdenSQL.Parameters.AddWithValue("@IdElemento", IdElemento);
+                    OrdenSQL.Parameters.AddWithValue("@NumeroPlaca", NumeroPlaca);
+                    OrdenSQL.Parameters.AddWithValue("@NumeroSerial ", NumeroSerial);
+                    OrdenSQL.Parameters.AddWithValue("@Marca", Marca);
+                    OrdenSQL.Parameters.AddWithValue("@Modelo", Modelo);
+                    OrdenSQL.Parameters.AddWithValue("@Descripcion", Descripcion);
+                    return OrdenSQL.ExecuteNonQuery();
+
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    cnx.Close();
+                    cnx.Dispose();
+                    OrdenSQL.Dispose();
+                }
+            }
+        }
+        public int EliminarElemento(Int64 IdElemento)
+        {
+            using (SqlConnection cnx = new SqlConnection(strconn))
+            {
+                cnx.Open();
+
+                SqlCommand OrdenSql = new SqlCommand("SpEliminarElemento", cnx);
+                OrdenSql.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    OrdenSql.Parameters.AddWithValue("@IdElemento", IdElemento);
+                    return OrdenSql.ExecuteNonQuery();
+
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    cnx.Close();
+                    cnx.Dispose();
+                    OrdenSql.Dispose();
+                }
+            }
+        }
+
+    }
+}
