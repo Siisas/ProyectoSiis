737412e26763fb643e6df22924ce2e6c2207a44c
Ricardo Lara <ricardo.lara06@hotmail.com>
2016-05-27 03:16:51 -0500

    ss

--- ProyectoSiis2/LayerData/LayerDataElementos.cs
+++ ProyectoSiis2/LayerData/LayerDataElementos.cs
@@ -10,7 +10,7 @@ namespace LayerData
 {
     public class LayerDataElementos
     {
-        public string strconn = @"Server=tcp:proyectosiis.database.windows.net,1433;Database=Proyecto1;User ID=Siis@proyectosiis;Password={123456789Aa;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+        public string strconn = @"Server=tcp:proyectosiis.database.windows.net,1433;Database=Proyecto1;User ID=Siis@proyectosiis;Password={123456789Aa];Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
 
         public LayerDataElementos() { }
 
